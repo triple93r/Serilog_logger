a50907936f4bee7e82ab90616e4dd0af6e873e18
triple93r <56282248+triple93r@users.noreply.github.com>
2023-11-01 08:58:32 +0530

    init34

--- LoggerApp/LoggerApp/Controllers/HomeController.cs
+++ LoggerApp/LoggerApp/Controllers/HomeController.cs
@@ -1,16 +1,21 @@
 ﻿using LoggerApp.Models;
 using Microsoft.AspNetCore.Mvc;
+using Serilog;
 using System.Diagnostics;
+using System.Net;
+using System.Web;
 
 namespace LoggerApp.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IWebHostEnvironment _webHostEnvironment;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IWebHostEnvironment webHostEnvironment)
         {
             _logger = logger;
+            _webHostEnvironment = webHostEnvironment;
         }
 
         public IActionResult Index()
@@ -22,6 +27,43 @@ namespace LoggerApp.Controllers
         public IActionResult Login()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(string usrname)
+        {
+            GetIPAddress(usrname, "login");
+            return View();
+        }
+
+        protected string GetIPAddress(string uname, string sesontyp)
+        {
+            string hostName2 = Dns.GetHostName();
+            IPHostEntry hostEntry2 = Dns.GetHostEntry(hostName2);
+
+            int v = 0;
+            string hh = "";
+            foreach (IPAddress address in hostEntry2.AddressList)
+            {
+                hh += (v > 0) ? "," + address.ToString() : address.ToString();
+                v++;
+            }
+            Log_tbl log_Tbl = new Log_tbl();
+            log_Tbl.ipaddress = hh;
+            log_Tbl.HostName = hostName2;
+            log_Tbl.UserName = uname;
+            log_Tbl.Module = "DBW";
+            log_Tbl.createdOn = DateTime.Now;
+            log_Tbl.createdBy = uname;
+            log_Tbl.remarks = sesontyp;
+
+            string contentRootPath = _webHostEnvironment.ContentRootPath;
+            string logpath = Path.Combine(contentRootPath, "Logs/logs.txt");
+
+            var log = new LoggerConfiguration().WriteTo.File(logpath).CreateLogger();
+      
+            log.Information("User : " + log_Tbl.UserName + ", Module : " + log_Tbl.Module + ", inTime : " + log_Tbl.createdOn + " Host : " + log_Tbl.HostName + ", ipAddress : " + log_Tbl.ipaddress + ", Remarks : " + log_Tbl.remarks);
+            return hh;
         }
 
         public IActionResult Privacy()
---
+++ LoggerApp/LoggerApp/Models/Log_tbl.cs
@@ -0,0 +1,14 @@
+﻿namespace LoggerApp.Models
+{
+    public class Log_tbl
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string Module { get; set; }
+        public string HostName { get; set; }
+        public string ipaddress { get; set; }
+        public DateTime createdOn { get; set; }
+        public string createdBy { get; set;}
+        public string remarks { get; set;}
+    }
+}
